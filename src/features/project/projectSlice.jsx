959bc707715a38f395712dddeb2386c7c88aae1a
nghia <nghianm2803@gmail.com>
2023-07-27 11:13:43 +0700

    fix bug _ref undefined create project

--- src/features/project/projectSlice.jsx
+++ src/features/project/projectSlice.jsx
@@ -58,24 +58,23 @@ const slice = createSlice({
 
 export const getProjects =
   ({ page, limit = PROJECTS_LIMIT_PER_PAGE, name }) =>
-  async (dispatch) => {
-    dispatch(slice.actions.startLoading());
-    try {
-      const params = { page, limit };
-      if (name) {
-        params.name = name;
-      }
-      const response = await apiService.get(`/projects`, {
-        params,
-      });
+    async (dispatch) => {
+      dispatch(slice.actions.startLoading());
+      try {
+        const params = { page, limit };
+        if (name) {
+          params.name = name;
+        }
+        const response = await apiService.get(`/projects`, {
+          params,
+        });
 
-      dispatch(slice.actions.getProjectSuccess(response.data));
-    } catch (error) {
-      console.error("Error occurred during API request:", error);
-      dispatch(slice.actions.hasError(error.message));
-      toast.error(error.message);
-    }
-  };
+        dispatch(slice.actions.getProjectSuccess(response.data));
+      } catch (error) {
+        dispatch(slice.actions.hasError(error.message));
+        toast.error(error.message);
+      }
+    };
 
 export const getSingleProject = (id) => async (dispatch) => {
   try {
@@ -91,53 +90,53 @@ export const getSingleProject = (id) => async (dispatch) => {
 };
 
 export const createProject =
-  ({ name, description }) =>
-  async (dispatch) => {
-    try {
-      dispatch(slice.actions.startLoading());
-      const response = await apiService.post("/projects/", {
-        name,
-        description,
-      });
-      dispatch(slice.actions.createProjectSuccess(response.data));
-      toast.success(response.data.message);
-      dispatch(getProjects());
-    } catch (error) {
-      dispatch(slice.actions.hasError(error.message));
-      toast.error(error.message);
-    }
-  };
+  ({ name, description, page, limit = PROJECTS_LIMIT_PER_PAGE }) =>
+    async (dispatch) => {
+      try {
+        dispatch(slice.actions.startLoading());
+        const response = await apiService.post("/projects", {
+          name,
+          description,
+        });
+        dispatch(slice.actions.createProjectSuccess(response.data));
+        toast.success(response.data.message);
+        dispatch(getProjects({ page, limit }));
+      } catch (error) {
+        dispatch(slice.actions.hasError(error.message));
+        toast.error(error.message);
+      }
+    };
 
 export const updateProject =
   ({ id, name, description }) =>
-  async (dispatch) => {
-    try {
-      dispatch(slice.actions.startLoading());
-      const data = { name, description };
-      const response = await apiService.put(`/projects/${id}`, data);
-      dispatch(slice.actions.updateProjectSuccess(response.data));
-      toast.success(response.data.message);
-      dispatch(getSingleProject(id));
-    } catch (error) {
-      dispatch(slice.actions.hasError(error.message));
-      toast.error(error.message);
-    }
-  };
+    async (dispatch) => {
+      try {
+        dispatch(slice.actions.startLoading());
+        const data = { name, description };
+        const response = await apiService.put(`/projects/${id}`, data);
+        dispatch(slice.actions.updateProjectSuccess(response.data));
+        toast.success(response.data.message);
+        dispatch(getSingleProject(id));
+      } catch (error) {
+        dispatch(slice.actions.hasError(error.message));
+        toast.error(error.message);
+      }
+    };
 
 export const deleteProject =
   ({ id }) =>
-  async (dispatch) => {
-    dispatch(slice.actions.startLoading());
-    try {
-      const response = await apiService.delete(`/projects/${id}`);
-      console.log("res", response);
-      dispatch(slice.actions.deleteProjectSuccess(response.data));
-      toast.success(response.data.message);
-      dispatch(getSingleProject(id));
-    } catch (error) {
-      dispatch(slice.actions.hasError(error.message));
-      toast.error(error.message);
-    }
-  };
+    async (dispatch) => {
+      dispatch(slice.actions.startLoading());
+      try {
+        const response = await apiService.delete(`/projects/${id}`);
+        console.log("res", response);
+        dispatch(slice.actions.deleteProjectSuccess(response.data));
+        toast.success(response.data.message);
+        dispatch(getSingleProject(id));
+      } catch (error) {
+        dispatch(slice.actions.hasError(error.message));
+        toast.error(error.message);
+      }
+    };
 
 export default slice.reducer;
